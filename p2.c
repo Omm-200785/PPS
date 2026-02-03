0904c0b770a52ac85733c03ce7319ca0b4b4941c
Omm Biswojeet Bhal <sahukirtika86@gmail.com>
2026-02-03 20:18:58 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,34 @@
+  GNU nano 7.2                                                            p3.c#include<stdio.h>
+int main()
+{
+      printf("\n algorithmic operations:");
+      int a,b;
+      printf("Enter the value of a & b:");
+      scanf("%d %d",&a,&b);
+      printf("Sum is:%d\n",a+b);
+       printf("Substraction is:%d\n",a-b);
+        printf("Multiplication is:%d\n",a*b);
+         printf("Division is:%d\n",a/b);
+          printf("Remainder is:%d\n",a%b);
+      return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                 [ Read 14 lines ]
+^G Help         ^O Write Out    ^W Where Is     ^K Cut          ^T Execute      ^C Location     M-U Undo        M-A Set Mark    M-] To Bracket
+^X Exit         ^R Read File    ^\ Replace      ^U Paste        ^J Justify      ^/ Go To Line   M-E Redo        M-6 Copy        ^Q Where Was
