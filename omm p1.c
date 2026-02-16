723df6901d0f9bd0af6bd3ad7dbfac51ceb696ff
Omm Biswojeet Bhal <sahukirtika86@gmail.com>
2026-02-16 10:34:23 +0530

    Delete omm p1.c

--- omm p1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int myNum;
-    printf("Type a number and press enter: \n");
-    scanf("%d", &myNum);
-    printf("Your number is: %d", myNum);
-    return 0.0;
-}
