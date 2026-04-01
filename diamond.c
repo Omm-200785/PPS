d5de1e1903d8cdfdf18e0f87ff6bb24f9ebf65e2
Omm Biswojeet Bhal <sahukirtika86@gmail.com>
2026-04-01 09:23:32 +0530

    Add files via upload

---
+++ diamond.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main() 
+{
+    int n = 10,i,j;
+    for(i=0; i<n; i++)
+    {
+         for(j=i; j<n; j++)
+        {
+            printf(" ");
+        }
+        for(j=1; j<=i; j++)
+        {
+            printf("%d",j);
+        }
+        for(j=i-1; j>=1; j--)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    for(i = n; i >= 1; i--) 
+    {
+        for(j = n; j > i; j--)
+        {
+            printf(" ");
+        }
+        for(j = 1; j <= i; j++) 
+        {
+            printf("%d", j);
+        }
+        for(j = i - 1; j >= 1; j--) 
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
